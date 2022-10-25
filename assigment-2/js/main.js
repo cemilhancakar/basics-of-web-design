a9d5f1b070c580c50bfdfc574a376a6230f077ad
cemilhancakar <87346820+cemilhancakar@users.noreply.github.com>
2022-10-25 04:22:47 +0300

    odev2

---
+++ assigment-2/js/main.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("sec1").style.backgroundColor = "#eaf9ff";
+  } else {
+    document.getElementById("sec1").style.backgroundColor="#fff";
+  }
+}
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
